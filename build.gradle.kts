6f7ba0f8326a6b92e36953df26dc03629cdd31eb
Raman Gupta <rocketraman@gmail.com>
2020-05-07 23:50:21 -0400

    Publish to jcenter and central via bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ import org.jetbrains.dokka.gradle.DokkaTask
 plugins {
   kotlin("jvm") version "1.3.71"
   id("org.jetbrains.dokka") version "0.10.0"
+  signing
   `maven-publish`
 }
 
-group = "org.kpropmap"
+group = "com.github.rocketraman"
 version = "0.0.2"
 
 repositories {
@@ -33,7 +34,7 @@ val dokka by tasks.getting(DokkaTask::class) {
 val dokkaJar by tasks.creating(Jar::class) {
   group = JavaBasePlugin.DOCUMENTATION_GROUP
   description = "Assembles Kotlin docs with Dokka"
-  classifier = "javadoc"
+  archiveClassifier.set("javadoc")
   // dependsOn(dokka) not needed; dependency automatically inferred by from(dokka)
   from(dokka)
 }
@@ -42,7 +43,7 @@ val dokkaJar by tasks.creating(Jar::class) {
 val sourcesJar by tasks.creating(Jar::class) {
   group = JavaBasePlugin.DOCUMENTATION_GROUP
   description = "Assembles sources JAR"
-  classifier = "sources"
+  archiveClassifier.set("sources")
   from(sourceSets["main"].allSource)
 }
 
@@ -56,19 +57,55 @@ tasks {
     useJUnitPlatform()
   }
 }
-/*
+
 publishing {
   publications {
-    create("default", MavenPublication::class.java) {
+    create<MavenPublication>("maven") {
+      groupId = "com.github.rocketraman"
       from(components["java"])
       artifact(sourcesJar)
       artifact(dokkaJar)
+      pom {
+        name.set("kpropmap")
+        description.set("Type-safe (ish) maps in Kotlin")
+        url.set("https://github.com/rocketraman/kpropmap")
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("https://opensource.org/licenses/MIT")
+          }
+        }
+        developers {
+          developer {
+            id.set("rocketraman")
+            name.set("Raman Gupta")
+            email.set("rocketraman@gmail.com")
+          }
+        }
+        scm {
+          connection.set("scm:git:git@github.com:rocketraman/kpropmap.git")
+          developerConnection.set("scm:git:ssh://github.com:rocketraman/kpropmap.git")
+          url.set("https://github.com/rocketraman/kpropmap")
+        }
+      }
     }
   }
   repositories {
     maven {
-      url = uri("$buildDir/repository")
+      name = "bintray"
+      val bintrayUsername = "rocketraman"
+      val bintrayRepoName = "maven"
+      val bintrayPackageName = "kpropmap"
+      setUrl("https://api.bintray.com/content/$bintrayUsername/$bintrayRepoName/$bintrayPackageName/${project.version};publish=0;override=1")
+      credentials {
+        username = project.findProperty("bintray_user") as String?
+        password = project.findProperty("bintray_api_key") as String?
+      }
     }
   }
 }
-*/
+
+signing {
+  useGpgCmd()
+  sign(publishing.publications["maven"])
+}
